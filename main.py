572e6980e762121a2c62002543b003892702f06f
unknown <s3975162@rmit.edu.vn>
2024-05-13 12:46:25 +0700

    continue chatting after result

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import json
 from os import getenv
 from dotenv import load_dotenv
+from random import randint
 
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
 import google.generativeai as genai
 
@@ -20,10 +22,17 @@ countries = json.loads(open('countries.json').read())
 genai.configure(api_key=getenv("GEMINI_API"))
 model = genai.GenerativeModel('gemini-pro')
 
+chats = {}
 
-def get_response(budget, travel_style, accommodation_standard, length_of_stay, country):
+
+def parse_prompt(budget, travel_style, accommodation_standard, length_of_stay, country):
+    prompt = f"recommend for me a travel plan. i have a budget of {budget}$, i plan to stay for {length_of_stay} days, {travel_style} travel style in {country} country with {accommodation_standard} accommodation standard"
+    return prompt
+
+
+def _get_response(budget, activity, travel_style, accommodation_standard, length_of_stay, country):
     response = model.generate_content(
-        f"recommend for me a travel plan. i have a budget of {budget}$, i plan to stay for {length_of_stay} days, {travel_style} travel style in {country} country with {accommodation_standard} accommodation standard")
+        f"Build for me a {length_of_stay} trip in {country} with a budget of {budget} $USD. My travel style is {travel_style}. I would like to visit {country}. I love {activity}. My ideal accommodation is {accommodation_standard}. Could you also recommend specific accommodations that match my interest?")
     return response.text
 
 
@@ -42,9 +51,26 @@ def read_form_get(request: Request):
     return templates.TemplateResponse(request=request, name="form.html")
 
 
-@app.post("/form", response_class=HTMLResponse)
+@app.post("/conversation", response_class=HTMLResponse)
 def read_form_post(request: Request, budget: float = Form(...), travel_style: str = Form(...), accommodation_standard: str = Form(...), length_of_stay: int = Form(...), country: str = Form(...)):
-    print(budget, travel_style, accommodation_standard, length_of_stay, country)
-    response = get_response(budget, travel_style,
-                            accommodation_standard, length_of_stay, country)
-    return templates.TemplateResponse(request=request, name="response.html", context={"response": response})
+    chat = model.start_chat(history=[])
+    while True:
+        chat_id = randint(0, 999999)
+        if chat_id not in chats:
+            chats[chat_id] = chat
+            break
+    response = chat.send_message(parse_prompt(
+        budget, travel_style, accommodation_standard, length_of_stay, country))
+    return templates.TemplateResponse(request=request, name="response.html", context={"chat_id": chat_id, "response": response.text})
+
+
+class Item(BaseModel):
+    chat_id: int
+    prompt: str
+
+
+@app.post("/chat")
+def read_chat(request: Request, item: Item):
+    chat = chats[item.chat_id]
+    response = chat.send_message(item.prompt)
+    return {"response": response.text}
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,76 +1,74 @@
 window.onload = function () {
-    if (!window.location.href.includes('form')) {
-        return
-    }
+    if (window.location.href.includes('form')) {
+        // list countries
+        var countryDropdown = document.querySelector(".country-dropdown")
 
-    // list countries
-    var countryDropdown = document.querySelector(".country-dropdown")
-
-    fetch('/get_countries', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .then(data => {
-            countries = data
-            for (var country in countries) {
-                var li = document.createElement("li")
-                var a = document.createElement("a")
-                a.classList.add("dropdown-item")
-                a.href = "#"
-                a.onclick = (function (country) {
-                    return function () {
-                        chooseCountry(country, countries[country])
-                    }
-                })(country);
-                a.textContent = countries[country]
-                li.appendChild(a)
-                countryDropdown.appendChild(li)
+        fetch('/get_countries', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
             }
         })
+            .then(response => response.json())
+            .then(data => {
+                countries = data
+                for (var country in countries) {
+                    var li = document.createElement("li")
+                    var a = document.createElement("a")
+                    a.classList.add("dropdown-item")
+                    a.href = "#"
+                    a.onclick = (function (country) {
+                        return function () {
+                            chooseCountry(country, countries[country])
+                        }
+                    })(country);
+                    a.textContent = countries[country]
+                    li.appendChild(a)
+                    countryDropdown.appendChild(li)
+                }
+            })
 
-    // pagination
-    var pages = Array.from(document.querySelectorAll('.page'))
-    var breadcrumbs = Array.from(document.querySelectorAll('.breadcrumbs-item'))
-    var currentIndex = 0
+        // pagination
+        var pages = Array.from(document.querySelectorAll('.page'))
+        var breadcrumbs = Array.from(document.querySelectorAll('.breadcrumbs-item'))
+        var currentIndex = 0
 
-    pages[currentIndex].classList.add('active')
+        pages[currentIndex].classList.add('active')
 
-    var nextButtons = Array.from(document.querySelectorAll('.next-button'))
-    nextButtons.forEach(function (button, index) {
-        button.addEventListener('click', function () {
-            if (pages[currentIndex].querySelector('.input-box').value === '') {
-                customAlert('Please fill in the required fields', 'alert-warning')
-            }
-            else {
+        var nextButtons = Array.from(document.querySelectorAll('.next-button'))
+        nextButtons.forEach(function (button, index) {
+            button.addEventListener('click', function () {
+                if (pages[currentIndex].querySelector('.input-box').value === '') {
+                    customAlert('Please fill in the required fields', 'alert-warning')
+                }
+                else {
+                    pages[currentIndex].classList.remove('active')
+                    breadcrumbs[currentIndex].classList.remove('is-active')
+                    currentIndex++
+                    if (currentIndex === 5) {
+                        pages[currentIndex].classList.add('active')
+                        setTimeout(function () {
+                            document.querySelector('.form_info').submit()
+                        }, 1000)
+                    } else {
+                        pages[currentIndex].classList.add('active')
+                        breadcrumbs[currentIndex].classList.add('is-active')
+                    }
+                }
+            })
+        })
+
+        var backButtons = Array.from(document.querySelectorAll('.back-button'))
+        backButtons.forEach(function (button, index) {
+            button.addEventListener('click', function () {
                 pages[currentIndex].classList.remove('active')
                 breadcrumbs[currentIndex].classList.remove('is-active')
-                currentIndex++
-                if (currentIndex === 5) {
-                    pages[currentIndex].classList.add('active')
-                    setTimeout(function () {
-                        document.querySelector('.form_info').submit()
-                    }, 1000)
-                } else {
-                    pages[currentIndex].classList.add('active')
-                    breadcrumbs[currentIndex].classList.add('is-active')
-                }
-            }
+                currentIndex--
+                pages[currentIndex].classList.add('active')
+                breadcrumbs[currentIndex].classList.add('is-active')
+            })
         })
-    })
-
-    var backButtons = Array.from(document.querySelectorAll('.back-button'))
-    backButtons.forEach(function (button, index) {
-        button.addEventListener('click', function () {
-            pages[currentIndex].classList.remove('active')
-            breadcrumbs[currentIndex].classList.remove('is-active')
-            currentIndex--
-            pages[currentIndex].classList.add('active')
-            breadcrumbs[currentIndex].classList.add('is-active')
-        })
-    })
+    }
 }
 
 function chooseCountry(country, name) {
@@ -117,4 +115,40 @@ function customAlert(message, type) {
         alertContainer.classList.remove(type)
         alertMsg.innerHTML = "";
     }, 5000);
+}
+
+function sendPrompt(chat_id) {
+    var userInput = document.getElementById('user-input').value
+    var idInt = parseInt(chat_id)
+
+    var userHtml = "<br><p><img src='/static/images/avatar.png' class='img-fluid' alt='avatar' width='30px' heigh='30px'>&nbsp;<strong>You</strong></p><p>" + userInput + "</p>"
+    document.getElementById('chat-messages').innerHTML += userHtml
+
+    var spinnerHtml = '<div id="spinner" class="spinner-border" style="width: 2rem; height: 2rem;" role="status"><span class="visually-hidden">Loading...</span></div>';
+    var responseHtml = "<br><p><img src='/static/images/avatar.png' class='img-fluid' alt='avatar' width='30px' heigh='30px'>&nbsp;<strong>Traveloke</strong></p>" + spinnerHtml
+    document.getElementById('chat-messages').innerHTML += responseHtml
+
+    var chatMessages = document.getElementById('chat-messages');
+    chatMessages.lastElementChild.scrollIntoView();
+
+    fetch('/chat', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ chat_id: idInt, prompt: userInput }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            var html = converter.makeHtml(data.response)
+            document.getElementById('spinner').outerHTML = html
+
+            var chatMessages = document.getElementById('chat-messages');
+            chatMessages.lastElementChild.scrollIntoView();
+        })
+        .catch((error) => {
+            console.error('Error:', error)
+        })
+
+    document.getElementById('user-input').value = ''
 }
